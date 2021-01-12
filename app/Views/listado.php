450ca532845a4827e0a7d79672f5de5ccec79f03
AnaBanana <cristinad.13cbo@gmail.com>
2021-01-12 03:11:41 -0600

    diseño de pantallas

--- app/Views/listado.php
+++ app/Views/listado.php
@@ -1,26 +1,34 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://localhost/CodeIgniterCRUD/styles/listado.css">
 
     <title>CRUD Pokemon!</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png" width="30" height="30" alt="">
+        <div class="nav">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png" width="30"
+                height="30" alt="">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <a class="nav-link">
+                        <?php echo session('usuario')?>
+                    </a>
+                </ul>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <a class="nav-link"><?php echo session('usuario')?></a>
-            </ul>
+                <a href="<?php echo base_url('/salir')?>"><button class="btn btn-danger btn-sm">Salir</button></a>
 
-            <a href="<?php echo base_url('/salir')?>"><button class="btn btn-outline-danger my-2 my-sm-0">Salir</button></a>
-
+            </div>
         </div>
     </nav>
 
@@ -31,6 +39,7 @@
                 <form method="POST" action="<?php echo base_url().'/crear' ?>">
                     <label for="mote">Mote</label>
                     <input type="text" name="mote" id="mote" class="form-control" required="">
+                    <br>
                     <label for="nivel">Nivel</label>
                     <input type="number" name="nivel" id="nivel" class="form-control" required="">
                     <br>
@@ -52,17 +61,25 @@
                             <th>Eliminar</th>
                         </tr>
                         <?php foreach($datos as $key): ?>
-                            <tr>
-                                <td><?php echo $key->id ?></td>
-                                <td><?php echo $key->mote ?></td>
-                                <td><?php echo $key->nivel ?></td>
-                                <td>
-                                    <a href="<?php echo base_url().'/obtenerPokemon/'.$key->id ?>" class="btn btn-warning btn-sm">Editar</a>
-                                </td>
-                                <td>
-                                    <a href="<?php echo base_url().'/eliminar/'.$key->id ?>" class="btn btn-danger btn-sm">Eliminar</a>
-                                </td>
-                            </tr>
+                        <tr>
+                            <td>
+                                <?php echo $key->id ?>
+                            </td>
+                            <td>
+                                <?php echo $key->mote ?>
+                            </td>
+                            <td>
+                                <?php echo $key->nivel ?>
+                            </td>
+                            <td>
+                                <a href="<?php echo base_url().'/obtenerPokemon/'.$key->id ?>"
+                                    class="btn btn-warning btn-sm">Editar</a>
+                            </td>
+                            <td>
+                                <a href="<?php echo base_url().'/eliminar/'.$key->id ?>"
+                                    class="btn btn-danger btn-sm">Eliminar</a>
+                            </td>
+                        </tr>
                         <?php endforeach; ?>
                     </table>
                 </div>
@@ -72,42 +89,39 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
     <script type="text/javascript">
         let mensaje = '<?php echo $mensaje ?>';
 
-        if(mensaje == '1')
-        {
-            swal(':D','Agregado con exito','success');
+        if (mensaje == '1') {
+            swal(':D', 'Agregado con exito', 'success');
         }
-        else if(mensaje == '0')
-        {
-            swal(':P','Error al agregar','error');
+        else if (mensaje == '0') {
+            swal(':P', 'Error al agregar', 'error');
         }
-        else if(mensaje == '2')
-        {
-            swal(':D','Actualizado con exito','success');
+        else if (mensaje == '2') {
+            swal(':D', 'Actualizado con exito', 'success');
         }
-        else if(mensaje == '3')
-        {
-            swal(':P','Error al actualizar','error');
+        else if (mensaje == '3') {
+            swal(':P', 'Error al actualizar', 'error');
         }
-        else if(mensaje == '4')
-        {
-            swal(':D','Eliminado con exito','success');
+        else if (mensaje == '4') {
+            swal(':D', 'Eliminado con exito', 'success');
         }
-        else if(mensaje == '5')
-        {
-            swal(':P','Error al eliminar','error');
-        }
-        else if(mensaje == '9')
-        {
-            swal(':D','Bienvenido a tu PC','success');
+        else if (mensaje == '5') {
+            swal(':P', 'Error al eliminar', 'error');
         }
     </script>
-  </body>
+</body>
+
 </html>
--- app/Views/login.php
+++ app/Views/login.php
@@ -1,43 +1,48 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Login</title>
-  </head>
-  <body>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="http://localhost/CodeIgniterCRUD/styles/login.css">
 
-    <div class="container">
-      
-      <div class="row">
-        <div class="col-sm-4"></div>
-        <div class="col-sm-4">
-          <h1>Login Pokemon</h1>
-          <form action="<?php echo base_url('/login') ?>" method="POST">
-            <label for="usuario">Correo</label>
-            <input type="text" name="usuario" required="" class="form-control">
-            <label for="password">Password</label>
-            <input type="password" name="password" required="" class="form-control">
-            <br>
-            <button class="btn btn-primary">Iniciar sesión</button>
-            <a href="<?php echo base_url().'/registro' ?>" class="btn btn-warning">Registrarse</a>
-          </form>
-          <br>
-          <a href="<?php echo base_url().'/recuperar' ?>">¿Olvidaste tu contraseña?</a>
+  <title>Login</title>
+</head>
+
+<body>
+  <div class="container">
+  
+        <div class="card text-left justify-content-md-center">
+          <div class="row">
+            <div class="col"></div>
+            <div class="col-9">
+              <h1>Login Pokemon</h1>
+              <p></p>
+              <form action="<?php echo base_url('/login') ?>" method="POST">
+                <label for="usuario">Correo</label>
+                <input type="text" name="usuario" required="" class="form-control">
+                <label for="password">Password</label>
+                <input type="password" name="password" required="" class="form-control">
+                <br>
+                <button class="btn btn-primary">Iniciar sesión</button>
+              </form>
+            </div>
+            <div class="col"></div>
+          </div>
         </div>
-        <div class="col-sm-4"></div>
-      </div>
-    </div>
+      
+    
+  </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
 </html>
